37ee5ef4dda11306cbc68f7022ed5d2b2eaaae28
Wesley Roberto <suporte@ubbonline.org.br>
2021-03-05 17:08:24 -0300

    aula 2

--- api-petshop/api/index.js
+++ api-petshop/api/index.js
@@ -1,7 +1,11 @@
 const express = require('express')
-const app= express()
-const bodyParser = require('body')
+const app = express()
+const bodyParser = require('body-parser')
+const config = require('config')
 
 app.use(bodyParser.json())
 
-app.listen(3000, () => console.log('A Api esta ok!'))
+const roteador = require('./rotas/fornecedores')
+app.use('/api/fornecedores', roteador)
+
+app.listen(config.get('api.porta'), () => console.log('A API está funcionando!'))
